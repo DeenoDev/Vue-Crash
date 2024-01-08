bc30dd7d60b0b41a529dd2178311471cdb93d95b
DeenoDev <deyanoklaasen@outlook.com>
2024-01-08 23:46:39 +0200

    entered more properties into the data() object.

--- random-user-gen/app.js
+++ random-user-gen/app.js
@@ -1,9 +1,13 @@
 const app = Vue.createApp({
     data(){
         return{
-            firstName: 'John'
+            firstName: 'John',
+            lastName: 'Doe',
+            email: 'john@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/men.jpg',
         }
-    }
+    },
 })
 
 app.mount('#app');
