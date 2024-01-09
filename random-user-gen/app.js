db8966e57fb7398804336ed3e9cb6e5042a5ac16
DeenoDev <deyanoklaasen@outlook.com>
2024-01-09 22:09:58 +0200

    allocated all variables to results object variables that was fetched.

--- random-user-gen/app.js
+++ random-user-gen/app.js
@@ -11,15 +11,15 @@ const app = Vue.createApp({
     methods: {
         async getUser(){
             const res = await fetch('https://randomuser.me/api');
-            const { results } = await res.json();
+            const { results } = await res.json(); //Destructured variable since res.json will return a results array with single user by default.
 
             console.log(results);
 
-            this.firstName = 'Sam';
-            this.lastName = 'Smith';
-            this.email = 'sam@gmail.com';
-            this.gender = 'female';
-            this.picture = 'https://randomuser.me/api/portraits/women/10.jpg';
+            this.firstName = results[0].name.first;
+            this.lastName = results[0].name.last;
+            this.email = results[0].email;
+            this.gender = results[0].gender;
+            this.picture = results[0].picture.thumbnail;
         },
     },
 })
