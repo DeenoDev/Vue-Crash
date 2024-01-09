85f298f0c26e4859aab9d32b9a5340da679b3452
DeenoDev <deyanoklaasen@outlook.com>
2024-01-09 21:40:32 +0200

    added new user data ro getUser function.

--- random-user-gen/app.js
+++ random-user-gen/app.js
@@ -5,12 +5,16 @@ const app = Vue.createApp({
             lastName: 'Doe',
             email: 'john@gmail.com',
             gender: 'male',
-            picture: 'https://randomuser.me/api/portraits/men.jpg',
+            picture: 'https://randomuser.me/api/portraits/men/10.jpg',
         }
     },
     methods: {
         getUser(){
-            console.log(this.firstName)
+            this.firstName = 'Sam'
+            this.lastName = 'Smith'
+            this.email = 'sam@gmail.com'
+            this.gender = 'female'
+            this.picture = 'https://randomuser.me/api/portraits/female/10.jpg',
         },
     },
 })
